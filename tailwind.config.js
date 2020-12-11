b3ec4a8a6611624f17c4d700ccab05c930199bfd
Maria Jose Mendez <majomendez88mena@gmail.com>
2020-12-11 15:25:06 -0600

    feat(tailwind): tailwind css config and set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = (isProd) => ({
     prefix: '',
     purge: {
       enabled: isProd,
       content: ['**/*.html', '**/*.ts']
     },
-    darkMode: false, // or 'media' or 'class'
+    darkMode: media, // or 'media' or 'class'
     theme: {
-      extend: {},
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        gray: colors.coolGray,
+        yellow: colors.amber,
+        green: colors.emerald,
+        blue: colors.lightBlue,
+        indigo: colors.indigo,
+        purple: colors.violet,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      },
     },
     variants: {
       extend: {},
